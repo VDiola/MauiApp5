4a28cd5024dd131680ff2e7e66275a087926c719
VDiola <126273351+VDiola@users.noreply.github.com>
2023-11-13 08:20:03 +0800

    Add files via upload

---
+++ TFTPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp5.Pages;
+
+public partial class TFTPage : ContentPage
+{
+	public TFTPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ValorantPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp5.Pages;
+
+public partial class ValorantPage : ContentPage
+{
+	public ValorantPage()
+	{
+		InitializeComponent();
+	}
+}
